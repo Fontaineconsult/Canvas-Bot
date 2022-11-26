b38b754c17a4fadbdb81818b56b66eaaeeaa3c31
Daniel Fontaine <fontaine@sfsu.edu>
2022-11-25 20:43:52 -0800

    before pull

--- canvas_page_classes/module.py
+++ canvas_page_classes/module.py
@@ -2,6 +2,7 @@ from core_canvas_classes.generic_page_component_node import PageComponentNode
 
 
 
+
 class Module(PageComponentNode):
 
     def __init__(self, session, component_html, tree_vis, parent, page_manifest, content_manifest,
